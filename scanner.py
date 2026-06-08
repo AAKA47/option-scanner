fa9e8822b0203e175b6a8d5607355770dd1a7a8a
AAKA47 <55848840+AAKA47@users.noreply.github.com>
2026-06-08 20:37:33 +0330

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,7 +1,7 @@
 import requests
 
-BOT_TOKEN = "توکن_ربات"
-CHAT_ID = "چت_آیدی"
+BOT_TOKEN = "8727264897:AAHKRXC4znJi3CG1j8yKZLn9z2jGov8F-Pg"
+CHAT_ID = "101936503"
 
 def send():
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
